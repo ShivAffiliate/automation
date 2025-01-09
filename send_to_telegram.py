da1120dbde85d2988370dc14b6243fc98f322f3a
ShivAffiliate <shivkumaraffiliate@gmail.com>
2025-01-09 12:18:44 +0530

    Update send_to_telegram.py

--- send_to_telegram.py
+++ send_to_telegram.py
@@ -1,8 +1,8 @@
 from telegram import Bot
 
 # Set up the bot with the token
-bot = Bot(token='YOUR_TELEGRAM_BOT_API_KEY')
+bot = Bot(token='7928675863:AAHF-5tTGWdJ-yw8MdWzgs_sJnyWSDwAApw')
 
 # Function to send messages to Telegram
 def send_to_telegram(message):
-    bot.send_message(chat_id='@YOUR_CHANNEL', text=message)
+    bot.send_message(chat_id='@TrendifyStoreBot', text=message)  # Replace @TrendifyStoreBot with your channel's chat ID
