f68c49ce8b43fc7a423730015afe7ea997965af1
ShivAffiliate <shivkumaraffiliate@gmail.com>
2025-01-31 11:07:20 +0530

    Update run_bot.sh

--- run_bot.sh
+++ run_bot.sh
@@ -9,13 +9,3 @@ if ! pgrep -f "python3 bot.py" > /dev/null; then
 else
     echo "bot.py is already running."
 fi
-
-# Check if python3 x.py is already running
-if ! pgrep -f "python3 x.py" > /dev/null; then
-    # If not running, start x.py
-    echo "Starting x.py..."
-    nohup python3 x.py > x.log 2>&1 &
-    echo "x.py started with PID $!"
-else
-    echo "x.py is already running."
-fi
